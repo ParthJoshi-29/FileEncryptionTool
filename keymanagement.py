0c3d514504480a9a20756c3d9aefe24d9fb9ff59
ManasB-10 <157683428+ManasB-10@users.noreply.github.com>
2025-04-18 16:23:45 +0530

    Delete keymanagement.py

--- keymanagement.py
+++
@@ -1,7 +0,0 @@
-def generate_key():
-    key = Fernet.generate_key()
-    with open(KEY_FILE, "wb") as key_file:
-        key_file.write(key)
-        
-def load_key():
-    return open(KEY_FILE, "rb").read()
